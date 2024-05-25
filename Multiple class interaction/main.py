93c67bca95d06c277d6ca163f0e227b2c0c00a3a
anoekkooijmans <a.kooijmans@st.hanze.nl>
2024-05-25 21:18:54 +0200

    Step 3a

--- Multiple class interaction/main.py
+++ Multiple class interaction/main.py
@@ -3,38 +3,7 @@ from Reader import Reader
 from AverageYear import AverageYear
 from AverageMonth import AverageMonth
 
-def step_1a():
-    keys = 'a,b,c,d'
-    vals = '3,2,1,5'
-    line = CsvConverter(header = keys, values = vals)
-    json_line = line.csv_to_json()
-    print(json_line)
-    
-def step_1b():
-    reader = Reader('dSST.csv')
-    print(reader.get_lines())  # Returns lines 2-6 as JSON
-    print(reader.get_lines())  # Returns lines 7-11 as JSON
-    print(reader.get_lines())
-    
-def step_2a():
-    average = AverageYear()
-    plt = average.create_plot()
-    plt.show()
-    plt.close()
-    
-def step_2b():
-    average = AverageMonth()
-    plt = average.create_plot()
-    plt.show()
-    plt.close()
-
-def step_3a():
-    reader = Reader('dSST.csv')
-    print(reader.get_lines())  # Returns lines 2-6 as JSON
-    print(reader.get_lines())  # Returns lines 7-11 as JSON
-    print(reader.get_lines())
-    
-def step_3b():
+def read_lines():
     prod = Reader('dSST.csv')
     cons1 = AverageYear()
     cons2 = AverageMonth()
@@ -43,5 +12,4 @@ def step_3b():
     print(prod.get_lines())
     
 if __name__ == "__main__":
-    step_3b()
-
+    read_lines()
