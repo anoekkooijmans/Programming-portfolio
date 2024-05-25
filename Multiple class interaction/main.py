3f7e9cf24252c36304555801bbb28fd8a08c7fa8
anoekkooijmans <a.kooijmans@st.hanze.nl>
2024-05-25 15:27:52 +0200

    Step 2a

--- Multiple class interaction/main.py
+++ Multiple class interaction/main.py
@@ -1,5 +1,6 @@
 from CsvConverter import CsvConverter
 from Reader import Reader
+from AverageYear import AverageYear
 
 def step_1a():
     keys = 'a,b,c,d'
@@ -13,7 +14,13 @@ def step_1b():
     print(reader.get_lines())  # Returns lines 2-6 as JSON
     print(reader.get_lines())  # Returns lines 7-11 as JSON
     print(reader.get_lines())
-        
+    
+def step_2():
+    average = AverageYear()
+    plt = average.calculate_average()
+    plt.show()
+    plt.close()
+    
 if __name__ == "__main__":
-    step_1b()
+    step_2()
 
