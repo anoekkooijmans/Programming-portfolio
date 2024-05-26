e597e37cb3463bee860dc386780a735b347ea78c
anoekkooijmans <a.kooijmans@st.hanze.nl>
2024-05-26 12:48:50 +0200

    Step 1: changing the Reader

--- Dependency injection/reader.py
+++ Dependency injection/reader.py
@@ -1,40 +1,51 @@
-import json 
 import linecache
-from tkinter import W
+import time
+import json
 
-class CsvToJson:
+class CsvConverter:
+    
+    def __init__(self, header, values):
+        self.header = header
+        self.values = values
+    
+    def csv_to_json(self):
+        
+        keys = self.header.strip("\n").split(",")
+        vals = self.values.strip("\n").split(",")
+        
+        json_dict = dict(zip(keys,vals))
+        json_string = json.dumps(json_dict)
 
-    def __init__(self, headers):
-        self.headers = headers.strip().split(',')
-
-    def csv_to_json(self, csv_rows):
-        json_val = []
-        for row in csv_rows:
-            row = row.split(',')
-            assert len(row) == len(self.headers), 'Length of data and headers should be the same'
-            json_val.append(dict(zip(self.headers, [float(x) for x in row])))
-        return json.dumps(json_val)
+        return json_string
 
 class Reader:
-    def __init__(self, filename, stride_length):
-        self.filename = filename
-        self.stride_length = stride_length
-        self.converter = CsvToJson(linecache.getline(self.filename, 1))
-
+    
+    def __init__(self, file_path = "dSST.csv", stride=5):
+        self.file_path = file_path
+        self.stride = stride
+        self.current_line = 2
+        self.header = linecache.getline(self.file_path, 1)
+        self.converter = CsvConverter(self.header,"")
+        
     def __iter__(self):
         return self
-
+    
     def __next__(self):
         lines = []
-        with open(self.filename, 'r') as file:
-            for _ in range(self.stride_length):
-                line = file.readline().strip()
-                if not line:
-                    break
-                lines.append(line)
-        if not lines:
-            raise StopIteration
-        return self.converter.csv_to_json(lines)
+        for i in range(self.stride):
+            line = linecache.getline(self.file_path, self.current_line)
+            if not line:
+                if lines:  # If there are lines collected, return them before stopping iteration
+                    return lines
+                else:  # If no lines are collected, stop iteration
+                    raise StopIteration
+
+            self.converter.values = line
+            lines.append(self.converter.csv_to_json())
+            self.current_line += 1
+
+        time.sleep(5) 
+        return lines
     
 # Add functionality to the Reader-class, so that the code below will work.
 if __name__=='__main__':
